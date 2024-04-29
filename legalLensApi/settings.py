ad2510f928a6bc67675b837afffd036b322f2829
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-04-29 20:02:40 +0000

    fixed database closing issue

--- legalLensApi/settings.py
+++ legalLensApi/settings.py
@@ -191,3 +191,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#CELERY SETTINGS
+
+CELERY_BROKER_URL = ''
+CELERYY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER='json'
+CELERY_TASK_SERIRALIZER='json'
