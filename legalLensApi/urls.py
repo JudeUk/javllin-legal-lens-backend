49ef10b48a90a6bd8da9cfe0770dc1fa38e170a0
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-05-01 16:35:36 +0000

    search backend fixed added text index search function

--- legalLensApi/urls.py
+++ legalLensApi/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 from caseApp.views import(CaseViewSet, create, upload_file)
 from chatLegal.views import(chat_constituition)
-from searchCases.views import search_case
+# from searchCases.views import case_search
+from searchCases.views import text_index_search_case
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,5 +15,6 @@ urlpatterns = [
     path("cases", create, name="cases"),
     path('upload/', upload_file, name="upload_file"),
     path('chat_constituition/', chat_constituition, name="chat_constituition"),
-    path('case_search/', search_case, name="search_case"),
+    # path('case_search/', case_search, name="case_search"),
+    path('case_search/', text_index_search_case, name="case_search"),
 ]
--- searchCases/views.py
+++ searchCases/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from pymongo import MongoClient
 import json
 
@@ -15,8 +17,10 @@ client = pymongo.MongoClient(MONGODB_URI)
 db = client.sample_mflix
 collection = db.cases
 
+@api_view(['POST'])
 @csrf_exempt
-def search_case(request):
+@permission_classes([AllowAny])
+def case_search(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         # case_id = data.get('case_id', None)
@@ -31,7 +35,7 @@ def search_case(request):
                 response_data = {
                     'case_number': case.get('case_number'),
                     'case_title': case.get('case_title'),
-                    'court': case.get('court'),
+                    'court': case.get('case_court'),
                     'date': case.get('date')
                 }
                 return JsonResponse(response_data)
@@ -44,6 +48,60 @@ def search_case(request):
     else:
         # If request method is not POST
         return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
+
+
+
+@csrf_exempt
+def text_index_search_case(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        # case_id = data.get('case_id', None)
+        case_title = data.get('case_title', None)
+
+        if case_title:
+            # Searching for the case in MongoDB
+            cases =  collection.find({"$text": {"$search": case_title}})
+
+            if cases:
+                # case = dict(case)
+                # Case found, returning the case details
+                # response_data = {
+                #     'case_number': case.get('case_number'),
+                #     'case_title': case.get('case_title'),
+                #     'court': case.get('court'),
+                #     'date': case.get('date')
+                # }
+                # return JsonResponse(response_data)
+                response_data = []
+                for case in cases:
+                  
+                  case_title = case.get('case_title')
+                  if case_title:
+                    case['case_title'] = case['case_title'].replace('\xa0', ' ')
+
+                    response_data.append({
+                        'case_number': case.get('case_number'),
+                        'case_title': case.get('case_title'),
+                        'court': case.get('court'),
+                        'date': case.get('date')
+                    })
+
+
+
+
+                print(response_data)
+                return JsonResponse(response_data, safe=False)
+            else:
+                # Case not found
+                return JsonResponse({'error': 'Case not found'}, status=404)
+        else:
+            # If case_id is not provided
+            return JsonResponse({'error': 'Case ID is required'}, status=400)
+    else:
+        # If request method is not POST
+        return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
+
+
 
 
 
